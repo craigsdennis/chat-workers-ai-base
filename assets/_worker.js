1dd1be6f706de382bab45500b10bcfb1ea158f7a
Craig Dennis <craigsdennis@gmail.com>
2024-03-06 18:25:37 -0800

    Bump

--- assets/_worker.js
+++ assets/_worker.js
@@ -4985,6 +4985,8 @@ app.post("/api/chat", async (c) => {
   if (payload?.config?.systemMessage) {
     messages.unshift({ role: "system", content: payload.config.systemMessage });
   }
+  console.log("Model", payload.config.model);
+  console.log("Messages", JSON.stringify(messages));
   const eventSourceStream = await ai.run(payload.config.model, { messages, stream: true });
   const tokenStream = eventSourceStream.pipeThrough(new TextDecoderStream()).pipeThrough(new EventSourceParserStream2());
   return streamText(c, async (stream2) => {
